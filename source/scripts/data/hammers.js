92d7800094aa05a2d78f4af615b20e8c74426981
Andrew Izosimov <chernigovsky108@gmail.com>
2024-09-09 13:58:23 +0300

    fixes links

--- source/scripts/data/hammers.js
+++ source/scripts/data/hammers.js
@@ -1,40 +1,38 @@
-import { replacePath } from './../functions/replace-path.js'
-
 export const hammers = [
   {
     id: 170,
     name: 'AR 45',
-    image: replacePath('images/catalog/attachments/anrui/AR45/AR45_foto_desktop@2x.webp'),
+    image: '/images/catalog/attachments/anrui/AR45/AR45_foto_desktop@2x.webp',
     property: 'Вес: ',
     propertyValue: '137',
     propertyUnit: 'кг',
-    link: replacePath('anrui/ar-45.html')
+    link: '/pages/anrui/ar-45.html'
   },
   {
     id: 200,
     name: 'AR 53',
-    image: replacePath('images/catalog/attachments/anrui/AR53/AR53_foto_desktop@2x.webp'),
+    image: '/images/catalog/attachments/anrui/AR53/AR53_foto_desktop@2x.webp',
     property: 'Вес: ',
     propertyValue: '180',
     propertyUnit: 'кг',
-    link: replacePath('anrui/ar-53.html')
+    link: '/pages/anrui/ar-53.html'
   },
   {
     id: 300,
     name: 'AR 68',
-    image: replacePath('images/catalog/attachments/anrui/AR68/AR68_foto_desktop@2x.webp'),
+    image: '/images/catalog/attachments/anrui/AR68/AR68_foto_desktop@2x.webp',
     property: 'Вес: ',
     propertyValue: '362',
     propertyUnit: 'кг',
-    link: replacePath('anrui/ar-68.html')
+    link: '/pages/anrui/ar-68.html'
   },
   {
     id: 500,
     name: 'AR 75',
-    image: replacePath('images/catalog/attachments/anrui/AR75/AR75_foto_desktop@2x.webp'),
+    image: '/images/catalog/attachments/anrui/AR75/AR75_foto_desktop@2x.webp',
     property: 'Вес: ',
     propertyValue: '466',
     propertyUnit: 'кг',
-    link: replacePath('anrui/ar-75.html')
+    link: '/pages/anrui/ar-75.html'
   }
 ];
