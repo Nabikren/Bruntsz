2d8d593bc57ee62b2731b83c1bfe9487124f7073
Andrew Izosimov <chernigovsky108@gmail.com>
2024-09-11 11:15:14 +0300

    fixes video

--- source/scripts/data/hammers.js
+++ source/scripts/data/hammers.js
@@ -34,5 +34,77 @@ export const hammers = [
     propertyValue: '466',
     propertyUnit: 'кг',
     link: '/pages/anrui/ar-75.html'
-  }
+  },
+  {
+    id: 501,
+    name: 'AR 85',
+    image: '/images/catalog/attachments/anrui/AR85/AR85_foto_desktop@2x.webp',
+    property: 'Вес: ',
+    propertyValue: '668',
+    propertyUnit: 'кг',
+    link: '/pages/anrui/ar-85.html'
+  },
+  {
+    id: 502,
+    name: 'AR 100',
+    image: '/images/catalog/attachments/anrui/AR100/AR100_foto_desktop@2x.webp',
+    property: 'Вес: ',
+    propertyValue: '986',
+    propertyUnit: 'кг',
+    link: '/pages/anrui/ar-100.html'
+  },
+  {
+    id: 503,
+    name: 'AR 140',
+    image: '/images/catalog/attachments/anrui/AR140/AR140_foto_desktop@2x.webp',
+    property: 'Вес: ',
+    propertyValue: '1910',
+    propertyUnit: 'кг',
+    link: '/pages/anrui/ar-140.html'
+  },
+  {
+    id: 504,
+    name: 'AR 155',
+    image: '/images/catalog/attachments/anrui/AR155/AR155_foto_desktop@2x.webp',
+    property: 'Вес: ',
+    propertyValue: '2610',
+    propertyUnit: 'кг',
+    link: '/pages/anrui/ar-155.html'
+  },
+  {
+    id: 505,
+    name: 'AR 165',
+    image: '/images/catalog/attachments/anrui/AR165/AR165_foto_desktop@2x.webp',
+    property: 'Вес: ',
+    propertyValue: '3149',
+    propertyUnit: 'кг',
+    link: '/pages/anrui/ar-165.html'
+  },
+  {
+    id: 506,
+    name: 'AR 175',
+    image: '/images/catalog/attachments/anrui/AR175/AR175_foto_desktop@2x.webp',
+    property: 'Вес: ',
+    propertyValue: '4575',
+    propertyUnit: 'кг',
+    link: '/pages/anrui/ar-175.html'
+  },
+  {
+    id: 507,
+    name: 'AR 20F',
+    image: '/images/catalog/attachments/anrui/AR20F/AR20F_foto_desktop@2x.webp',
+    property: 'Вес: ',
+    propertyValue: '466',
+    propertyUnit: 'кг',
+    link: '/pages/anrui/ar-20f.html'
+  },
+  {
+    id: 508,
+    name: 'AR 40F',
+    image: '/images/catalog/attachments/anrui/AR40F/AR40F_foto_desktop@2x.webp',
+    property: 'Вес: ',
+    propertyValue: '466',
+    propertyUnit: 'кг',
+    link: '/pages/anrui/ar-40f.html'
+  },
 ];
