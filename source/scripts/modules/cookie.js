c0da46515be309f6d366c987811dcc4522a380f5
Владимир Симонов <2172395@gmail.com>
2024-09-13 09:21:59 +0300

    finish

---
+++ source/scripts/modules/cookie.js
@@ -0,0 +1,32 @@
+export function setCookie(name, value, days) {
+  const expires = new Date();
+  expires.setTime(expires.getTime() + days * 24 * 60 * 60 * 1000);
+  document.cookie = `${name}=${value};expires=${expires.toUTCString()};path=/`;
+}
+
+// Получение куки
+export function getCookie(name) {
+  const cookies = document.cookie.split(';');
+  for (let i = 0; i < cookies.length; i++) {
+    const cookie = cookies[i].trim();
+    if (cookie.startsWith(name + '=')) {
+      return cookie.substring(name.length + 1);
+    }
+  }
+  return null;
+}
+
+// Проверка наличия куки и отображение уведомления
+export function checkCookie() {
+  const cookieConsent = getCookie('cookie_consent');
+  if (!cookieConsent) {
+    // Показываем окно уведомления
+    document.querySelector('.cookie-popup').style.display = 'inline-block';
+  }
+}
+
+// Скрытие окна уведомления и установка куки
+export function acceptCookies() {
+  setCookie('cookie_consent', 'accepted', 30); // Устанавливаем куки на 30 дней
+  document.querySelector('.cookie-popup').style.display = 'none';
+}
