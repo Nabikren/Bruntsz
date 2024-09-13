ba508dffaf13c9bdf36ef2e29c347d209320acf1
Izosimov_Andrew <chernigovsky108@gmail.com>
2024-09-13 12:56:37 +0300

    fix

--- source/php/send_email.php
+++ source/php/send_email.php
@@ -2,69 +2,51 @@
 <?php
 error_reporting(E_ERROR);
 
+// Инициализация переменных
+$username = $userphone = $useremail = $usercompany = $usercomments = $sab = '';
+
+// Проверка и обработка входящих данных
 if (isset($_POST['username'])) {
-  $username = $_POST['username'];
-  if ($username == '') {
-    unset($username);
-  }
+  $username = trim($_POST['username']);
 }
 if (isset($_POST['userphone'])) {
-  $userphone = $_POST['userphone'];
-  if ($userphone == '') {
-    unset($userphone);
-  }
+  $userphone = trim($_POST['userphone']);
 }
 if (isset($_POST['useremail'])) {
-  $useremail = $_POST['useremail'];
-  if ($useremail == '') {
-    unset($useremail);
-  }
+  $useremail = trim($_POST['useremail']);
 }
 if (isset($_POST['usercompany'])) {
-  $usercompany = $_POST['usercompany'];
-  if ($usercompany == '') {
-    unset($usercompany);
-  }
+  $usercompany = trim($_POST['usercompany']);
 }
 if (isset($_POST['usercomments'])) {
-  $usercomments = $_POST['usercomments'];
-  if ($usercomments == '') {
-    unset($usercomments);
-  }
+  $usercomments = trim($_POST['usercomments']);
 }
 if (isset($_POST['sab'])) {
-  $sab = $_POST['sab'];
-  if ($sab == '') {
-    unset($sab);
-  }
+  $sab = trim($_POST['sab']);
 }
 
-if (isset($username)) {
-  $username = stripslashes($username);
-  $username = htmlspecialchars($username);
-}
-if (isset($userphone)) {
-  $userphone = stripslashes($userphone);
-  $userphone = htmlspecialchars($userphone);
-}
-if (isset($useremail)) {
-  $useremail = stripslashes($useremail);
-  $useremail = htmlspecialchars($useremail);
-}
-if (isset($usercompany)) {
-  $usercompany = stripslashes($usercompany);
-  $usercompany = htmlspecialchars($usercompany);
-}
-if (isset($usercomments)) {
-  $usercomments = stripslashes($usercomments);
-  $usercomments = htmlspecialchars($usercomments);
+// Проверка на пустые поля
+if (empty($username) || empty($userphone) || empty($useremail)) {
+  echo "<div style='max-width:1024px; margin:0 auto; padding:0 20px; text-align: center;'>
+          <p style='color:red;'>Пожалуйста, заполните все поля перед отправкой.</p>
+        </div>";
+  echo "<a style='font-size: 24px; text-align: center' href='https://brunt-sz.ru'>Вернуться на главную</a>";
+  exit; // Прекращаем выполнение скрипта
 }
 
+// Экранирование входящих данных
+$username = htmlspecialchars(stripslashes($username));
+$userphone = htmlspecialchars(stripslashes($userphone));
+$useremail = htmlspecialchars(stripslashes($useremail));
+$usercompany = htmlspecialchars(stripslashes($usercompany));
+$usercomments = htmlspecialchars(stripslashes($usercomments));
+
+// Формирование сообщения
 $message = "
 Имя: $username \r\n
 Телефон: $userphone \r\n
 Почта: $useremail \r\n
-Комания: $usercompany  \r\n
+Компания: $usercompany \r\n
 Комментарии: $usercomments
 ";
 
@@ -74,9 +56,16 @@ $headers = 'From: order@brunt-sz.ru' . "\r\n" .
   'Reply-To: order@brunt-sz.ru' . "\r\n" .
   'X-Mailer: PHP/' . phpversion();
 
-mail('brunt-sz@mail.ru', 'Регистрация заявки - Brunt', $message, $headers);
-echo "<div style='max-width:1024px; margin:0 auto; padding:0 20px; text-align: center;'>
-<p style='color:green;'>Уважаемый(ая) <b style='color:red; font-size:24px'>$username</b> Ваше письмо отправленно успешно. Спасибо, наш менеджер с Вами свяжется.</p>
-</div>";
-echo "<a style='font-size: 24px; text-align: center' href='https://brunt-sz.ru'>Вернуться на главную</a>"
-  ?>
+// Отправка письма
+if (mail('2172395@gmail.com', 'Регистрация заявки - Brunt', $message, $headers)) {
+  echo "<div style='max-width:1024px; margin:0 auto; padding:0 20px; text-align: center;'>
+          <p style='color:green;'>Уважаемый(ая) <b style='color:red; font-size:24px'>$username</b>, Ваше письмо отправлено успешно. Спасибо, наш менеджер с Вами свяжется.</p>
+        </div>";
+} else {
+  echo "<div style='max-width:1024px; margin:0 auto; padding:0 20px; text-align: center;'>
+          <p style='color:red;'>Произошла ошибка при отправке сообщения. Пожалуйста, попробуйте позже.</p>
+        </div>";
+}
+
+echo "<a style='font-size: 24px; text-align: center' href='https://brunt-sz.ru'>Вернуться на главную</a>";
+?>
